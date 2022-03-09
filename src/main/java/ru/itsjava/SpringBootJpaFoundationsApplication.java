d42990d7b483f02d51e35c8fe44077e381046dab
Vitaliy <SeekerOfConfessor@gmail.com>
2022-03-10 02:30:14 +0300

    spring data

--- src/main/java/ru/itsjava/SpringBootJpaFoundationsApplication.java
+++ src/main/java/ru/itsjava/SpringBootJpaFoundationsApplication.java
@@ -4,14 +4,12 @@ import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.transaction.annotation.Transactional;
 import ru.itsjava.domain.Genre;
 import ru.itsjava.repository.FilmRepository;
 import ru.itsjava.repository.GenreRepository;
 
 import java.sql.SQLException;
 
-@Transactional
 @SpringBootApplication
 public class SpringBootJpaFoundationsApplication {
 
@@ -22,21 +20,27 @@ public class SpringBootJpaFoundationsApplication {
         System.out.println("genreRepository.getById(1L) = " + genreRepository.getById(1L));
 
         Genre genre = new Genre(0L, "western");
-        genreRepository.insert(genre);
+        genreRepository.save(genre);
         System.out.println("genreRepository.getById(3L) = " + genreRepository.getById(3L));
 
         Genre genre3 = genreRepository.getById(3L);
         genre3.setName("WESTERN");
-        genreRepository.update(genre3);
+        genreRepository.save(genre3);
         System.out.println("genreRepository.getById(3L) = " + genreRepository.getById(3L));
 
         genreRepository.deleteById(3L);
-        System.out.println("genreRepository.getById(3L) = " + genreRepository.getById(3L));
+        System.out.println("genreRepository.findById(3L).isPresent() = " + genreRepository.findById(3L).isPresent());
 
         FilmRepository filmRepository = context.getBean(FilmRepository.class);
         System.out.println(filmRepository.findAll());
 
         System.out.println("filmRepository.getById(1L) = " + filmRepository.getById(1L));
         //  Console.main(args);
+
+        //System.out.println("genreRepository.getByName(\"WESTERN\") = " + genreRepository.getByName("WESTERN"));
+        System.out.println("genreRepository.getByName(\"WESTERN\").isPresent() = " + genreRepository.getByName("WESTERN").isPresent());
+
+        System.out.println("filmRepository.findFilmByTitleAndGenre(\"Harry Potter\", genreRepository.getById(1L)).isPresent() = " +
+                filmRepository.findFilmByTitleAndGenre("Harry Potter", genreRepository.getById(1L)).isPresent());
     }
 }
